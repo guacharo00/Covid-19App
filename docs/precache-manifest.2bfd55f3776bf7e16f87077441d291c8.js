5e54dcbbb9d47b07ea6e63f42cb1103329e74b17
Luis Fructuoso <lfcafn@gmail.com>
2020-07-21 21:13:45 -0400

    image bug repared

---
+++ docs/precache-manifest.2bfd55f3776bf7e16f87077441d291c8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72f57d34926b8e80953f5ac90cd2f03b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8bf5303c2c1ba7874be3",
+    "url": "/static/css/main.35f99881.chunk.css"
+  },
+  {
+    "revision": "035dc4aa655e01c133ca",
+    "url": "/static/js/2.20132eea.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/static/js/2.20132eea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8bf5303c2c1ba7874be3",
+    "url": "/static/js/main.f125ffa2.chunk.js"
+  },
+  {
+    "revision": "8311566eb2c4032cd971",
+    "url": "/static/js/runtime-main.a23cfd3e.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/static/media/image.d7265326.png"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/covid.luisfructuoso.com/precache-manifest.a32fd59e1c3e67ac7fa02b61de668be2.js"
+  "/precache-manifest.2bfd55f3776bf7e16f87077441d291c8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/covid.luisfructuoso.com/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
